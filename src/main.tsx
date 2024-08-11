77f3156e4ae3307e735d9d30c8bb92aa518a5a78
Arno van Staden <arnovanstaden@gmail.com>
2024-08-11 12:35:40 +0200

    add answers + mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Root from "./routes/root";
+import Root from "./routes/layout";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './error-page';
 
--- src/routes/root.tsx
+++
@@ -1,9 +0,0 @@
-const Root: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-};
-
-export default Root;
